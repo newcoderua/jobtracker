cb88ef3ddf7e31e1a61ac117a9f3db60152d0624
vladyslav <tradestadnyk@gmail.com>
2020-02-08 18:24:55 -0600

    adding initial functionality

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,356 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+import { Line, Pie } from "react-chartjs-2";
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col,
+  Button,
+  Collapse,
+  Table,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  InputGroupText,
+  Badge
+} from "reactstrap";
+// core components
+import {
+  dashboard24HoursPerformanceChart,
+  dashboardEmailStatisticsChart,
+  dashboardNASDAQChart
+} from "variables/charts.jsx";
+import './Dashboard.css'
+// import Button from 'reactstrap/Button'
+// import Collapse from 'reactstrap/Collapse'
+// import Card from 'reactstrap/Card'
+// import CardBody from 'reactstrap/CardBody'
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isAppliedOpen: true,
+      isSortByOpen: false,
+      isFiltersOpen: false
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <div className="add-new-job-application-button-content">
+            <div className="add-new-job-application-button">
+              <Button color="danger" onClick={() => {}}>+</Button>
+              <span>Add New</span>
+            </div>
+          </div>
+          <div className="main-content-filters">
+            <div className="main-content-filters-search">
+              <InputGroup className="no-border">
+                <Input placeholder="Search..." />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText>
+                    <i className="nc-icon nc-zoom-split" />
+                  </InputGroupText>
+                </InputGroupAddon>
+              </InputGroup>
+            </div>
+            <div className="main-content-filters-sortby-filters">
+              <div className="main-content-filters-sortby">
+                <ButtonDropdown isOpen={this.state.isSortByOpen} toggle={() => this.setState({ isSortByOpen: !this.state.isSortByOpen })}>
+                  <DropdownToggle caret>
+                    Sort By
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem>Another Action</DropdownItem>
+                    <DropdownItem>Another Action</DropdownItem>
+                    <DropdownItem>Another Action</DropdownItem>
+                  </DropdownMenu>
+                </ButtonDropdown>
+              </div>
+              <div className="main-content-filters-filters">
+                <Button onClick={() => this.setState({ isFiltersOpen: !this.state.isFiltersOpen })}>All Filters</Button>
+                <Modal isOpen={this.state.isFiltersOpen} toggle={() => this.setState({ isFiltersOpen: !this.state.isFiltersOpen })}>
+                  <ModalHeader toggle={() => this.setState({ isFiltersOpen: !this.state.isFiltersOpen })}>Modal title</ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={() => this.setState({ isFiltersOpen: !this.state.isFiltersOpen })}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={() => this.setState({ isFiltersOpen: !this.state.isFiltersOpen })}>Cancel</Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+          </div>
+          <div className="main-content-applied-jobs">
+            <Button color="primary" onClick={() => this.setState({ isAppliedOpen: !this.state.isAppliedOpen })} className="content-tab-button">
+              Applied
+              <Badge color="dark" className="main-content-applied-jobs-button-badge"> 3 </Badge>
+            </Button>
+            <Collapse isOpen={this.state.isAppliedOpen} className="content-main-applications-table">
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Collapse>
+          </div>
+          <div className="main-content-phone-screen-jobs">
+            <Button color="warning" onClick={() => {}} className="content-tab-button">Phone Screen</Button>
+          </div>
+          <div className="main-content-in-person-interview-jobs">
+            <Button color="success" onClick={() => {}} className="content-tab-button">In Person Interview</Button>
+          </div>
+          <div className="main-content-rejected-jobs">
+            <Button color="info" onClick={() => {}} className="content-tab-button">Rejected</Button>
+          </div>
+          {/* <Row>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-globe text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Capacity</p>
+                        <CardTitle tag="p">150GB</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Update Now
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-money-coins text-success" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Revenue</p>
+                        <CardTitle tag="p">$ 1,345</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="far fa-calendar" /> Last day
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-vector text-danger" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Errors</p>
+                        <CardTitle tag="p">23</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="far fa-clock" /> In the last hour
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-favourite-28 text-primary" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Followers</p>
+                        <CardTitle tag="p">+45K</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Update now
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row> */}
+          {/* <Row>
+            <Col md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Users Behavior</CardTitle>
+                  <p className="card-category">24 Hours performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboard24HoursPerformanceChart.data}
+                    options={dashboard24HoursPerformanceChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-history" /> Updated 3 minutes ago
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="4">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Email Statistics</CardTitle>
+                  <p className="card-category">Last Campaign Performance</p>
+                </CardHeader>
+                <CardBody>
+                  <Pie
+                    data={dashboardEmailStatisticsChart.data}
+                    options={dashboardEmailStatisticsChart.options}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="legend">
+                    <i className="fa fa-circle text-primary" /> Opened{" "}
+                    <i className="fa fa-circle text-warning" /> Read{" "}
+                    <i className="fa fa-circle text-danger" /> Deleted{" "}
+                    <i className="fa fa-circle text-gray" /> Unopened
+                  </div>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-calendar" /> Number of emails sent
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="8">
+              <Card className="card-chart">
+                <CardHeader>
+                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
+                  <p className="card-category">Line Chart with Points</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboardNASDAQChart.data}
+                    options={dashboardNASDAQChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="chart-legend">
+                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
+                    <i className="fa fa-circle text-warning" /> BMW 5 Series
+                  </div>
+                  <hr />
+                  <div className="card-stats">
+                    <i className="fa fa-check" /> Data information certified
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row> */}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
